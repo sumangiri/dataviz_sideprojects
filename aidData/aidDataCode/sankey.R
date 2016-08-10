049df1b1789cfee0d5bc1fd251d929afe488a8d2
Suman Giri <sumangiri@Sumans-MacBook-Pro.local>
2016-08-10 01:19:28 -0400

    fixed security issue

--- aidData/aidDataCode/sankey.R
+++ aidData/aidDataCode/sankey.R
@@ -7,6 +7,6 @@ nodes=nodes['id']
 d3Sankey(Nodes = nodes, Links = links, Source = 'source',
          Target = 'target', Value = 'weight', NodeID = 'id',
          width = 650, height = 450, fontsize = 12,
-         standAlone = TRUE, iframe = TRUE, file = '../../Downloads/demo_clf.html')
+         standAlone = FALSE, iframe = FALSE, file = '../../Downloads/demo_clf.html')
 
 
